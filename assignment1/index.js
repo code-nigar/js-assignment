83667755d3c5cb5d6895ccf5b0754ad4d0408283
Sharjeel Sultan <sharjeelsultan27@gmail.com>
2022-05-11 21:46:00 +0500

    JavaScript Exercises - chapter 1 & 2
    this commit includes my exercises as a beginner to JavaScript.

---
+++ assignment1/index.js
@@ -0,0 +1,26 @@
+//chapter 2
+//task 1
+var username;
+//task 2
+var myname= "Sharjeel Sultan";
+//task 3
+var message="hello world";
+alert(message);
+//task 4
+username = prompt("what is your user name",myname);
+var age= prompt("enter your age");
+var program = prompt("which program you are enrolled in ?");
+alert(username);
+alert(age);
+alert(program);
+//task 5
+alert("pizza\n"+"izza\n"+"zza\n"+"za\n"+"a");
+//task 6
+var email="sharjeelsultan27@gmail.com"
+alert("my email address is "+email);
+//task 7
+var book="A smarter way to learn JavaScript"
+alert("i am reading "+book);
+//task 9
+var pattern="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(pattern);
