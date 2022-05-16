a4b36e729d17097f9d15fb123b8e987667971b56
Sharjeel Sultan <sharjeelsultan27@gmail.com>
2022-05-16 16:10:46 +0500

    assignment 2 - chapter 3-4

---
+++ assignment2/chapter-4 tasks/app.js
@@ -0,0 +1,29 @@
+//task 1
+var numbers, $, _;
+numbers="numbers";
+$="$";
+_="_";
+
+//task 2
+//legal variables
+var $my_1stVariable="$my_1stVariable";
+var _2ndLegalVar;
+var thirdLegalVar;
+var fourth_legal_var;
+var fifth$legal_variable;
+//illegal variables:
+/* 
+var 1stvar;
+var 2nd_var;
+var alert;
+var forth variable;
+var fifth-variable;
+*/
+
+//task 3
+console.log(
+    "Rules for naming JS Variable \n\n"+
+    "variable names can only contain "+
+    numbers +", "+$ +", "+_ +"."
+    +"\n for example: "+ $my_1stVariable
+);
